a604ce2da8f1facdc2f92ce594c0c532fecefc99
Ringlings <110815694+Ringlings@users.noreply.github.com>
2022-11-14 17:19:36 +1000

    Setup

--- src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
+++ src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
 package com.example.example_mod.mixin;
 
-import com.example.example_mod.ExampleMod;
+import io.github.sweetberrycollective.SweetCrops;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		SweetCrops.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
